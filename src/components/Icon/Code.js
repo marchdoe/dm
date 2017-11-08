02ae4cc029b1200d0dd7512ff2d9b46f861e7212
Doug March <dougmarch@gmail.com>
2017-11-08 16:09:43 -0500

    add code icon for items that have a link to source code

---
+++ src/components/Icon/Code.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CodeIcon = ({ iconWidth, iconHeight }) => {
+  return (
+    <svg id="CodeIcon" xmlns="http://www.w3.org/2000/svg" width={iconWidth} height={iconHeight} viewBox="0 0 32 32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3">
+      <path d="M10 9 L3 17 10 25 M22 9 L29 17 22 25 M18 7 L14 27" />
+    </svg>
+  )
+}
+
+CodeIcon.propTypes = {
+  iconWidth: PropTypes.number,
+  iconHeight: PropTypes.number
+}
+
+CodeIcon.defaultProps = {
+  iconWidth: 18,
+  iconHeight: 18
+}
+
+export default CodeIcon
--- src/containers/Posts/index.js
+++ src/containers/Posts/index.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import styled from 'styled-components'
 import { fetchPosts } from '../../actions/index'
 
+import CodeIcon from '../../components/Icon/Code'
+
 import {
   Box,
   Heading,
@@ -18,13 +20,18 @@ const Wrapper = styled(Box)`
 `
 
 const StyledArticle = styled(Box)`
-  border-bottom: 2px dotted ${color.gray1};
+  border-bottom: 2px dotted ${color.gray2};
 
   &:last-child { border: 0; }
 `
 
 const StyledHeading = styled(Heading)`
   color: #666;
+`
+
+const CodeSourceLink = styled(Link)`
+  margin-left: 7px;
+  display: inline-block;
 `
 
 class Posts extends Component {
@@ -36,14 +43,16 @@ class Posts extends Component {
   renderPosts () {
     return this.props.posts.map((post, index) => {
 
+      const hasCodeSource = post.fields.github
       const hasExcerpt = post.fields.excerpt
 
       return (
         <StyledArticle py={[3, 3, 4]} key={post.sys.id}>
           <StyledHeading f={4} is={'h2'}>
-            <Link href={post.fields.source}>
+            <Link href={post.fields.source} target='_blank'>
               {post.fields.title}
             </Link>
+            {hasCodeSource ? (<CodeSourceLink href={post.fields.github} target='_blank' pt={2} f={2}><CodeIcon /></CodeSourceLink>) : ('')}
             {hasExcerpt ? (<Text pt={2} f={2}>{post.fields.excerpt}</Text>) : ('')}
           </StyledHeading>
         </StyledArticle>
